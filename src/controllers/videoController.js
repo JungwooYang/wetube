c8a66297aff72040ec06c4b3bae860d63b249460
JungwooYang <euistgh@gmail.com>
2021-11-15 11:59:06 +0900

    templates pug
    configuring pug
    partials
    extending
    varibles
    mvp
    conditionals

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,8 +1,11 @@
-export const trending = (req, res) => res.send("Trending videos");
-export const see = (req, res) => {
-    return res.send(`Watch video #${req.params.id}`)
+const fakeUser = {
+    username:"Jungwoo",
+    loggedIn: false,
 };
-export const edit = (req, res) => res.send("Edit video");
+
+export const trending = (req, res) => res.render("home", { pageTitle: "Home", fakeUser});
+export const see = (req, res) => res.render("watch", {pageTitle: "Watch"});
+export const edit = (req, res) => res.render("edit", {pageTitle: "Edit"});
 export const search = (req, res) => res.send("Search video");
 export const upload = (req, res) => res.send("Upload");
 export const deleteVideo = (req, res) => res.send("Delete video");
