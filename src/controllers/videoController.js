2d619ee87e80a1ab76abca44cc7a3683014bceb5
JungwooYang <euistgh@gmail.com>
2021-11-23 09:16:58 +0900

    edit/404/upload/watch

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -4,17 +4,33 @@ export const home = async (req, res) => {
     const videos = await Video.find({});
     return res.render("home", { pageTitle: "Home", videos });
 };
-export const watch = (req, res) => {
+export const watch = async (req, res) => {
     const { id } = req.params;
-    return res.render("watch", {pageTitle: `Watching` });
+    const video = await Video.findById(id);
+    if(!video){
+        return res.render("404", {pageTitle: "Video not found."});
+    }
+    return res.render("watch", {pageTitle: video.title, video });
 };
-export const getEdit = (req, res) => {
+export const getEdit = async (req, res) => {
     const { id } = req.params;
-    return res.render("edit", {pageTitle: `Editing` });
+    const video = await Video.findById(id);
+    if(!video){
+        return res.render("404", {pageTitle: "Video not found."});
+    }
+    return res.render("edit", {pageTitle: `Edit: ${video.title}`, video });
 };
-export const postEdit = (req, res) => {
+export const postEdit = async (req, res) => {
     const { id } = req.params;
-    const { title } = req.body;
+    const { title, description, hashtags } = req.body;
+    const video = await Video.findById(id);
+    if(!video){
+        return res.render("404", {pageTitle: "Video not found."});
+    }
+    video.title = title;
+    video.description = description;
+    video.hashtags = hashtags.split(",").map((word) => `#${word}`);
+    await video.save();
     return res.redirect(`/videos/${id}`);
 };
 
@@ -28,16 +44,12 @@ export const postUpload = async (req, res) => {
         await Video.create({
             title,
             description,
-            createdAt: Date.now(),
-            hashtags: hashtags.split(",").map((word) => `#${word}`),
-            meta: {
-                views: 0,
-                rating: 0,
-            },
+            hashtags: hashtags
+            .split(",")
+            .map((word) => (word.startswith("#") ? word : `#${word}`)),
         });
         return res.redirect("/");
     } catch(error){
-        console.log(error);
         return res.render("upload", {
             pageTitle: "Upload Video", 
             errorMessage: error._message,
