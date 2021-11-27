521156ac0e4bfab214ad8408fc966af52428bd2d
JungwooYang <euistgh@gmail.com>
2021-11-28 00:09:10 +0900

    github login

--- src/models/User.js
+++ src/models/User.js
@@ -2,10 +2,11 @@ import bcrypt from "bcrypt";
 import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
-  email: { type: String, unique: true, required:true},
-  username: { type: String, unique: true, required:true},
-  password: { type: String, required:true},
-  name: { type: String, required:true},
+  email: { type: String, unique: true, required:true },
+  socialOnly: { type: Boolean, default: false },
+  username: { type: String, unique: true, required:true },
+  password: { type: String },
+  name: { type: String, required:true },
   location: String,
 });
  
