94392449798ae6120e8c62d4acd5a1e0b340b310
JungwooYang <euistgh@gmail.com>
2021-12-03 22:21:38 +0900

    Update videoPlayer.js

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -4,7 +4,7 @@ const mute = document.getElementById("mute");
 const time = document.getElementById("time");
 const volume = document.getElementById("volume");
 
-const handlePlay = (e) => {
+const handlePlayClick = (e) => {
   // if the video is playing, pause it
   if (video.paused) {
     playBtn.innertext = "Pause";
@@ -20,7 +20,7 @@ const handleMute = (e) => {
   if (mute !== 0) {
   }
 };
-playBtn.addEventListener("click", handlePlay);
+playBtn.addEventListener("click", handlePlayClick);
 mute.addEventListener("click", handleMute);
 video.addEventListener("pause", handlePause);
 video.addEventListener("play", handlePlay);
