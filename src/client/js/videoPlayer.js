32cf3f95b7b0e5b3b40601349e6013cbfb49142c
JungwooYang <euistgh@gmail.com>
2021-12-05 17:29:56 +0900

    video player volume

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -2,25 +2,47 @@ const video = document.querySelector("video");
 const playBtn = document.getElementById("play");
 const mute = document.getElementById("mute");
 const time = document.getElementById("time");
-const volume = document.getElementById("volume");
+const volumeRange = document.getElementById("volume");
+
+let volumeValue = 0.5;
+video.volume = volumeValue;
 
 const handlePlayClick = (e) => {
   // if the video is playing, pause it
   if (video.paused) {
-    playBtn.innertext = "Pause";
     video.play();
   } else {
     video.pause();
   }
+  playBtn.innerText = video.paused ? "Play" : "Pause";
 };
 
-const handlePause = () => (playBtn.innerText = "Play");
-const handlePlay = () => (playBtn.innerText = "Pause");
-const handleMute = (e) => {
-  if (mute !== 0) {
+const handleMuteClick = (e) => {
+  if (video.muted) {
+    video.muted = false;
+  } else {
+    video.muted = true;
+  }
+  muteBtn.innerText = video.muted ? "Unmute" : "Mute";
+  volumeRange.value = video.muted ? 0 : volumeValue;
+};
+
+const handleVolumeChange = (event) => {
+  const {
+    target: { value },
+  } = event;
+  if (video.muted) {
+    video.muted = false;
+    muteBtn.innerText = "Mute";
+  }
+  volumeValue = value;
+  video.volume = value;
+
+  if (video.volume === 0) {
+    video.muted = true;
+    muteBtn.innerText = "Unmute";
   }
 };
 playBtn.addEventListener("click", handlePlayClick);
 mute.addEventListener("click", handleMute);
-video.addEventListener("pause", handlePause);
-video.addEventListener("play", handlePlay);
+volumeRange.addEventListener("input", handleVolumeChange);
